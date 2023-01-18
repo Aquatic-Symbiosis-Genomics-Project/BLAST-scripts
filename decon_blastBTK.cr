02b6113de51628e842b4ef868590ad5d900450d1
epaule <mh6@sanger.ac.uk>
2023-01-18 17:18:27 +0000

    reformatted with crystal tool format

--- decon_blastBTK.cr
+++ decon_blastBTK.cr
@@ -7,38 +7,38 @@ require "klib"
 
 include Klib
 
-btk=""
-fasta=""
-outdir="test"
+btk = ""
+fasta = ""
+outdir = "test"
 
 OptionParser.parse do |parser|
-	parser.banner = "Usage: decon_blastBTK --btk BTK_CSV_FILE --fasta FASTAFILE --outdir OUT_DIR"
-	parser.on("-b BTK_CSV_FILE","--btk=BTK_CSV_FILE","BTK CSV file"){|b|btk=b}
-	parser.on("-f FASTA_FILE","--fasta=FASTA_FILE","FASTA file of the assembly (can be compressed)"){|f|fasta=f}
-	parser.on("-o OUT_DIR","--outdir=OUT_DIR","output directory"){|o|outdir=o}
-	parser.on("-h", "--help", "Show this help") do
-		puts parser
-		exit
-	end
-  	parser.invalid_option do |flag|
-    		STDERR.puts "ERROR: #{flag} is not a valid option."
-		STDERR.puts parser
-		exit(1)
-  	end
+  parser.banner = "Usage: decon_blastBTK --btk BTK_CSV_FILE --fasta FASTAFILE --outdir OUT_DIR"
+  parser.on("-b BTK_CSV_FILE", "--btk=BTK_CSV_FILE", "BTK CSV file") { |b| btk = b }
+  parser.on("-f FASTA_FILE", "--fasta=FASTA_FILE", "FASTA file of the assembly (can be compressed)") { |f| fasta = f }
+  parser.on("-o OUT_DIR", "--outdir=OUT_DIR", "output directory") { |o| outdir = o }
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
 end
 
 def parse_btk_file(file : String)
-	i = [] of String
-	header=0
-	File.each_line(file) do |line|
-		header += 1
-		next if header == 1
-		if /TRUE/i.match(line)
-			columns=line.delete('"').split(',')
-			i << columns[-1]
-		end
-	end
-	return i
+  i = [] of String
+  header = 0
+  File.each_line(file) do |line|
+    header += 1
+    next if header == 1
+    if /TRUE/i.match(line)
+      columns = line.delete('"').split(',')
+      i << columns[-1]
+    end
+  end
+  return i
 end
 
 ids = parse_btk_file(btk)
@@ -47,13 +47,13 @@ Dir.mkdir_p(outdir)
 
 fp = GzipReader.new(fasta)
 fx = FastxReader.new(fp)
-fx.each{|e|
- 	if ids.includes? e.name
-		file="#{outdir}/#{e.name}.fa"
-		File.open(file,"w"){|f|
-	 		f.puts ">#{e.name}"
-			f.puts e.seq
-		}
-		system("curl -T #{file} http://172.27.25.136:35227 > #{file}.blast_out")
- 	end
+fx.each { |e|
+  if ids.includes? e.name
+    file = "#{outdir}/#{e.name}.fa"
+    File.open(file, "w") { |f|
+      f.puts ">#{e.name}"
+      f.puts e.seq
+    }
+    system("curl -T #{file} http://172.27.25.136:35227 > #{file}.blast_out")
+  end
 }
