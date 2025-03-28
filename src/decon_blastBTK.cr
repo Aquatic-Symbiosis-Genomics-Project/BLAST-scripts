13574c5a3aa5cf43e54eca3b721e42dfb1f93ede
Michael Paulini <mh6@sanger.ac.uk>
2025-03-28 10:25:10 +0000

    added a logic to resume

--- src/decon_blastBTK.cr
+++ src/decon_blastBTK.cr
@@ -66,6 +66,8 @@ fx = FastxReader.new(fp)
 fx.each { |e|
   if ids.includes? e.name
     file = "#{outdir}/#{e.name}.fa"
+    outf = "#{file}.blast_out"
+    next if File.exists?(outf) && File.empty?(outf)
     File.open(file, "w") { |f|
       f.puts ">#{e.name}"
       f.puts e.seq
